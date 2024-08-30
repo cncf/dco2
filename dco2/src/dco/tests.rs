310cb7947b41b8f3c882c19c39c9c2b0f41edc8f
Sergio Castaño Arteaga <tegioz@icloud.com>
2024-08-30 15:38:39 +0200

    Add some more tests
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- dco2/src/dco/tests.rs
+++ dco2/src/dco/tests.rs
@@ -1,5 +1,10 @@
+use std::vec;
+
 use super::{check, CheckInput};
-use crate::github::{Commit, GitUser};
+use crate::{
+    dco::{CommitCheckOutput, CommitError},
+    github::{Commit, GitUser},
+};
 use indoc::indoc;
 
 #[test]
@@ -205,4 +210,198 @@ fn check_valid_signoff_single_commit_email_contains_plus_alias() {
     let output = check(&input);
 
     assert!(output.commits_with_errors.is_empty());
+}
+
+#[test]
+fn check_invalid_author_email_single_commit() {
+    let commit1 = Commit {
+        author: Some(GitUser {
+            name: "user1".to_string(),
+            email: "invalid".to_string(),
+            ..Default::default()
+        }),
+        message: indoc! {r"
+            Test commit message
+
+            Signed-off-by: user1 <invalid>
+        "}
+        .to_string(),
+        ..Default::default()
+    };
+
+    let input = CheckInput {
+        commits: vec![commit1.clone()],
+    };
+    let output = check(&input);
+
+    assert_eq!(
+        output.commits_with_errors,
+        vec![CommitCheckOutput {
+            commit: commit1,
+            errors: vec![CommitError::InvalidAuthorEmail],
+        }]
+    );
+}
+
+#[test]
+fn check_invalid_author_email_and_no_signoff_single_commit() {
+    let commit1 = Commit {
+        author: Some(GitUser {
+            name: "user1".to_string(),
+            email: "invalid".to_string(),
+            ..Default::default()
+        }),
+        message: "Test commit message".to_string(),
+        ..Default::default()
+    };
+
+    let input = CheckInput {
+        commits: vec![commit1.clone()],
+    };
+    let output = check(&input);
+
+    assert_eq!(
+        output.commits_with_errors,
+        vec![CommitCheckOutput {
+            commit: commit1,
+            errors: vec![CommitError::InvalidAuthorEmail, CommitError::SignOffNotFound],
+        }]
+    );
+}
+
+#[test]
+fn check_invalid_committer_email_single_commit() {
+    let commit1 = Commit {
+        committer: Some(GitUser {
+            name: "user1".to_string(),
+            email: "invalid".to_string(),
+            ..Default::default()
+        }),
+        message: indoc! {r"
+            Test commit message
+
+            Signed-off-by: user1 <invalid>
+        "}
+        .to_string(),
+        ..Default::default()
+    };
+
+    let input = CheckInput {
+        commits: vec![commit1.clone()],
+    };
+    let output = check(&input);
+
+    assert_eq!(
+        output.commits_with_errors,
+        vec![CommitCheckOutput {
+            commit: commit1,
+            errors: vec![CommitError::InvalidCommitterEmail],
+        }]
+    );
+}
+
+#[test]
+fn check_invalid_author_and_committer_email_single_commit_same_email() {
+    let commit1 = Commit {
+        author: Some(GitUser {
+            name: "user1".to_string(),
+            email: "invalid".to_string(),
+            ..Default::default()
+        }),
+        committer: Some(GitUser {
+            name: "user1".to_string(),
+            email: "invalid".to_string(),
+            ..Default::default()
+        }),
+        message: indoc! {r"
+            Test commit message
+
+            Signed-off-by: user1 <user1@email.test>
+        "}
+        .to_string(),
+        ..Default::default()
+    };
+
+    let input = CheckInput {
+        commits: vec![commit1.clone()],
+    };
+    let output = check(&input);
+
+    assert_eq!(
+        output.commits_with_errors,
+        vec![CommitCheckOutput {
+            commit: commit1,
+            errors: vec![CommitError::InvalidCommitterEmail],
+        }]
+    );
+}
+
+#[test]
+fn check_invalid_author_and_committer_email_single_commit_different_emails() {
+    let commit1 = Commit {
+        author: Some(GitUser {
+            name: "user1".to_string(),
+            email: "invalid".to_string(),
+            ..Default::default()
+        }),
+        committer: Some(GitUser {
+            name: "user1".to_string(),
+            email: "invalid2".to_string(),
+            ..Default::default()
+        }),
+        message: indoc! {r"
+            Test commit message
+
+            Signed-off-by: user1 <user1@email.test>
+        "}
+        .to_string(),
+        ..Default::default()
+    };
+
+    let input = CheckInput {
+        commits: vec![commit1.clone()],
+    };
+    let output = check(&input);
+
+    assert_eq!(
+        output.commits_with_errors,
+        vec![CommitCheckOutput {
+            commit: commit1,
+            errors: vec![
+                CommitError::InvalidCommitterEmail,
+                CommitError::InvalidAuthorEmail
+            ],
+        }]
+    );
+}
+
+#[test]
+fn check_signoff_not_found_single_commit() {
+    let commit1 = Commit {
+        author: Some(GitUser {
+            name: "user1".to_string(),
+            email: "user1@email.test".to_string(),
+            ..Default::default()
+        }),
+        committer: Some(GitUser {
+            name: "user1".to_string(),
+            email: "user1@email.test".to_string(),
+            ..Default::default()
+        }),
+        message: "Test commit message".to_string(),
+        ..Default::default()
+    };
+
+    let input = CheckInput {
+        commits: vec![commit1.clone()],
+    };
+    let output = check(&input);
+
+    assert_eq!(
+        output.commits_with_errors,
+        vec![CommitCheckOutput {
+            commit: commit1,
+            errors: vec![CommitError::SignOffNotFound],
+        }]
+    );
 }
