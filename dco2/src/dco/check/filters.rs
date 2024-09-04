999d641a8330848f5862361737e2d3d8898bf026
Sergio Castaño Arteaga <tegioz@icloud.com>
2024-09-04 10:36:53 +0200

    Include all commits in check output
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- dco2/src/dco/check/filters.rs
+++ dco2/src/dco/check/filters.rs
@@ -4,65 +4,53 @@ use super::{CommitCheckOutput, CommitError};
 
 /// Template filter to check if any of the commits contain any of the
 /// provided errors.
-pub(crate) fn contains_error(
-    commits_with_errors: &[CommitCheckOutput],
-    errors: &[CommitError],
-) -> askama::Result<bool> {
-    Ok(commits_with_errors.iter().any(|c| c.errors.iter().any(|e| errors.contains(e))))
-}
-
-/// Template filter to check if all the commits contain the same errors.
-pub(crate) fn have_same_errors(commits_with_errors: &[CommitCheckOutput]) -> askama::Result<bool> {
-    for i in 1..commits_with_errors.len() {
-        if commits_with_errors[i].errors != commits_with_errors[0].errors {
-            return Ok(false);
-        }
-    }
-    Ok(true)
+pub(crate) fn contains_error(commits: &[CommitCheckOutput], errors: &[CommitError]) -> askama::Result<bool> {
+    Ok(commits.iter().any(|c| c.errors.iter().any(|e| errors.contains(e))))
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::dco::check::{
-        filters::{contains_error, have_same_errors},
-        CommitCheckOutput, CommitError,
-    };
+    use crate::dco::check::{filters::contains_error, CommitCheckOutput, CommitError};
 
     #[test]
     fn contains_error_one_commit_error_found() {
-        let commits_with_errors = vec![CommitCheckOutput {
+        let commits = vec![CommitCheckOutput {
             commit: Default::default(),
             errors: vec![CommitError::InvalidAuthorEmail],
+            success_reason: None,
         }];
 
-        assert!(contains_error(&commits_with_errors, &[CommitError::InvalidAuthorEmail]).unwrap());
+        assert!(contains_error(&commits, &[CommitError::InvalidAuthorEmail]).unwrap());
     }
 
     #[test]
     fn contains_error_one_commit_error_not_found() {
-        let commits_with_errors = vec![CommitCheckOutput {
+        let commits = vec![CommitCheckOutput {
             commit: Default::default(),
             errors: vec![CommitError::InvalidAuthorEmail],
+            success_reason: None,
         }];
 
-        assert!(!contains_error(&commits_with_errors, &[CommitError::InvalidCommitterEmail]).unwrap());
+        assert!(!contains_error(&commits, &[CommitError::InvalidCommitterEmail]).unwrap());
     }
 
     #[test]
     fn contains_error_two_commits_error_found() {
-        let commits_with_errors = vec![
+        let commits = vec![
             CommitCheckOutput {
                 commit: Default::default(),
                 errors: vec![CommitError::InvalidAuthorEmail],
+                success_reason: None,
             },
             CommitCheckOutput {
                 commit: Default::default(),
                 errors: vec![CommitError::InvalidCommitterEmail],
+                success_reason: None,
             },
         ];
 
         assert!(contains_error(
-            &commits_with_errors,
+            &commits,
             &[CommitError::InvalidAuthorEmail, CommitError::SignOffNotFound]
         )
         .unwrap());
@@ -70,122 +58,23 @@ mod tests {
 
     #[test]
     fn contains_error_two_commits_error_not_found() {
-        let commits_with_errors = vec![
+        let commits = vec![
             CommitCheckOutput {
                 commit: Default::default(),
                 errors: vec![CommitError::InvalidAuthorEmail],
+                success_reason: None,
             },
             CommitCheckOutput {
                 commit: Default::default(),
                 errors: vec![CommitError::InvalidCommitterEmail],
+                success_reason: None,
             },
         ];
 
         assert!(!contains_error(
-            &commits_with_errors,
+            &commits,
             &[CommitError::SignOffNotFound, CommitError::SignOffMismatch]
         )
         .unwrap());
-    }
-
-    #[test]
-    fn have_same_errors_one_commit_always_true() {
-        let commits_with_errors = vec![CommitCheckOutput {
-            commit: Default::default(),
-            errors: vec![CommitError::InvalidAuthorEmail],
-        }];
-
-        assert!(have_same_errors(&commits_with_errors).unwrap());
-    }
-
-    #[test]
-    fn have_same_errors_two_commits_one_error_matches() {
-        let commits_with_errors = vec![
-            CommitCheckOutput {
-                commit: Default::default(),
-                errors: vec![CommitError::InvalidAuthorEmail],
-            },
-            CommitCheckOutput {
-                commit: Default::default(),
-                errors: vec![CommitError::InvalidAuthorEmail],
-            },
-        ];
-
-        assert!(have_same_errors(&commits_with_errors).unwrap());
-    }
-
-    #[test]
-    fn have_same_errors_two_commits_two_errors_match() {
-        let commits_with_errors = vec![
-            CommitCheckOutput {
-                commit: Default::default(),
-                errors: vec![CommitError::InvalidAuthorEmail, CommitError::SignOffNotFound],
-            },
-            CommitCheckOutput {
-                commit: Default::default(),
-                errors: vec![CommitError::InvalidAuthorEmail, CommitError::SignOffNotFound],
-            },
-        ];
-
-        assert!(have_same_errors(&commits_with_errors).unwrap());
-    }
-
-    #[test]
-    fn have_same_errors_two_commits_two_errors_do_not_match() {
-        let commits_with_errors = vec![
-            CommitCheckOutput {
-                commit: Default::default(),
-                errors: vec![
-                    CommitError::InvalidAuthorEmail,
-                    CommitError::InvalidCommitterEmail,
-                ],
-            },
-            CommitCheckOutput {
-                commit: Default::default(),
-                errors: vec![CommitError::InvalidAuthorEmail, CommitError::SignOffNotFound],
-            },
-        ];
-
-        assert!(!have_same_errors(&commits_with_errors).unwrap());
-    }
-
-    #[test]
-    fn have_same_errors_three_commits_two_errors_match() {
-        let commits_with_errors = vec![
-            CommitCheckOutput {
-                commit: Default::default(),
-                errors: vec![CommitError::InvalidAuthorEmail, CommitError::SignOffNotFound],
-            },
-            CommitCheckOutput {
-                commit: Default::default(),
-                errors: vec![CommitError::InvalidAuthorEmail, CommitError::SignOffNotFound],
-            },
-            CommitCheckOutput {
-                commit: Default::default(),
-                errors: vec![CommitError::InvalidAuthorEmail, CommitError::SignOffNotFound],
-            },
-        ];
-
-        assert!(have_same_errors(&commits_with_errors).unwrap());
-    }
-
-    #[test]
-    fn have_same_errors_three_commits_one_error_does_not_match() {
-        let commits_with_errors = vec![
-            CommitCheckOutput {
-                commit: Default::default(),
-                errors: vec![CommitError::InvalidAuthorEmail],
-            },
-            CommitCheckOutput {
-                commit: Default::default(),
-                errors: vec![CommitError::SignOffNotFound],
-            },
-            CommitCheckOutput {
-                commit: Default::default(),
-                errors: vec![CommitError::InvalidAuthorEmail],
-            },
-        ];
-
-        assert!(!have_same_errors(&commits_with_errors).unwrap());
     }
 }
--- dco2/templates/output.md
+++ dco2/templates/output.md
@@ -1,13 +1,9 @@
-{% if commits_with_errors.is_empty() -%}
+{% if num_commits_with_errors == 0 -%}
 All commits are signed off!
 {% else -%}
 There is at least one commit incorrectly signed off. This means that the author of this commit failed to include a Signed-off-by line in the commit message.
 {% endif -%}
 
-{% if commits_with_errors|contains_error([CommitError::SignOffNotFound, CommitError::SignOffMismatch]) %}
-Tada!
-{% endif %}
-
-{% if commits_with_errors|have_same_errors %}
+{% if commits|contains_error([CommitError::SignOffNotFound, CommitError::SignOffMismatch]) %}
 Tada!
 {% endif %}
